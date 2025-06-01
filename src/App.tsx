b35abdb82a179835bdd3fc4f446fb3924fca32e5
omerhor <omer.hortig@gmail.com>
2025-06-01 11:38:19 -0400

    Pages will now always appear at the top

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import About from './pages/About';
 import OurMission from './pages/OurMission';
@@ -12,6 +13,7 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="app">
         <Header />
         <main className="main-content">
