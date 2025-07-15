d0415ce93ce2ebf0509bca20a483840e4f07beb6
omerhor <omer.hortig@gmail.com>
2025-07-15 00:01:47 -0400

    Read Vite port from .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { config } from 'dotenv'
+
+// Load environment variables
+config()
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(process.env.VITE_PORT) || 3000,
+  },
 })
