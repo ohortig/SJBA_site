b257da7549cb69b1f25d4ed95b74ff18d2b382e7
Omer Hortig <omer.hortig@gmail.com>
2026-01-22 16:37:39 -0500

    Change public bucket URLs from constants to environment variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL as string;
+export const BOARD_IMAGES_BUCKET = import.meta.env.VITE_BOARD_IMAGES_BUCKET as string;
+export const EVENT_FLYERS_BUCKET = import.meta.env.VITE_EVENT_FLYERS_BUCKET as string;
 
-export const BOARD_IMAGES_BUCKET =
-  'https://ivhsrdfhjxtrxvrwswuk.supabase.co/storage/v1/object/public/board-headshots/';
