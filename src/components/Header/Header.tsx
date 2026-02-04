33244bf2056cc8de5493b378cc151fc5286f1ad0
Omer Hortig <omer.hortig@gmail.com>
2026-02-04 15:28:01 -0500

    Fix lint issues

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -33,8 +33,16 @@ export const Header = () => {
       <nav className="nav-container">
         <div className="logo">
           <Link to="/" onClick={closeMenu}>
-            <img src="/sjba/sjba-logo-full.png" alt="SJBA Logo" className="logo-image logo-desktop" />
-            <img src="/sjba/sjba-logo-clear.png" alt="SJBA Logo" className="logo-image logo-mobile" />
+            <img
+              src="/sjba/sjba-logo-full.png"
+              alt="SJBA Logo"
+              className="logo-image logo-desktop"
+            />
+            <img
+              src="/sjba/sjba-logo-clear.png"
+              alt="SJBA Logo"
+              className="logo-image logo-mobile"
+            />
           </Link>
         </div>
 
